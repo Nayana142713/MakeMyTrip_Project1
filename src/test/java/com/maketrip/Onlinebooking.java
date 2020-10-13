7a89921225f3e146769a9d660d8c3cc34de5aa07
Nayana142713 <72754954+Nayana142713@users.noreply.github.com>
2020-10-13 07:45:50 +0530

    Update Onlinebooking.java

--- src/test/java/com/maketrip/Onlinebooking.java
+++ src/test/java/com/maketrip/Onlinebooking.java
@@ -35,20 +35,13 @@ public class Onlinebooking {
   @Test
   public void Test() throws InterruptedException {
 	  
-	 // driver.get("https://www.makemytrip.com");
-		//Thread.sleep(2000);
-	   // driver.switchTo().frame("webklipper-publisher-widget-container-notification-frame");
-		//Thread.sleep(3000);
-		//driver.findElement(By.xpath("//*[@id=\"webklipper-publisher-widget-container-notification-close-div\"]/i")).click();
-		//Thread.sleep(2000);
-		
+	 	
 		driver.findElement(By.xpath("//*[@id=\"SW\"]/div[1]/div[1]/ul/li[6]")).click();
 		
 		
 		WebElement input=driver.findElement(By.id("username"));
 		input.sendKeys("lechukarthika95@gmail.com");
-		//Thread.sleep(6000);
-		
+			
 		WebElement Continue=driver.findElement(By.xpath("//*[@id=\"SW\"]/div[1]/div[2]/div[2]/section/form/div[2]/button"));
 		Continue.click();
 		
@@ -57,15 +50,14 @@ public class Onlinebooking {
 		
 		
 		WebElement inputpassword=driver.findElement(By.xpath("//*[@id=\"password\"]"));
-	    inputpassword.sendKeys("Sep@1234");
+	        inputpassword.sendKeys("Sep@1234");
 		
 		WebElement signin=driver.findElement(By.xpath("//*[@id=\"SW\"]/div[1]/div[2]/div[2]/section/form/div[2]/button"));
 		signin.click();
 		
 		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//li[@class='   menu_Hotels ']/a")));
 		
-		//WebElement HotelBuild=driver.findElement((By.xpath("//*[@id=\"SW\"]/div[1]/div[2]/div/div/nav/ul/li[2]/a/span[1]")));
-		WebElement Hotel=driver.findElement((By.xpath("//li[@class='   menu_Hotels ']/a")));
+	        WebElement Hotel=driver.findElement((By.xpath("//li[@class='   menu_Hotels ']/a")));
 		Hotel.click();
 		
 		WebElement city=driver.findElement(By.id("city"));
@@ -98,50 +90,26 @@ public class Onlinebooking {
 		Thread.sleep(6000);
 		
 		 WebElement survey1=driver.findElement(By.xpath("//*[@id=\"survey-close-div\"]"));
-	     survey1.click();
+	         survey1.click();
 		
 		Thread.sleep(3000);
 		
 		Actions action = new Actions(driver);
 		WebElement slider =driver.findElement(By.xpath("//*[@id=\"hlistpg_fr_price_per_night\"]/div[2]/div[2]/div/span[1]/div"));
-		//wait.until(ExpectedConditions.elementToBeClickable(slider));
 		action.moveToElement(slider).click().dragAndDropBy(slider, +50, 0).build().perform();
-		//Thread.sleep(5000);
+		Thread.sleep(5000);
 			
 
 		Actions loca = new Actions(driver);
-		//WebElement select=driver.findElement(By.xpath("//*[@id=\"_Hlisting_area\"]/div[3]/div[1]/div/p/span"));
-	    // select.click();
 		WebElement locality=driver.findElement(By.xpath("//*[@id=\"hlistpg_fr_locality\"]/div/div[1]/ul/li[1]/span/label"));
 		loca.moveToElement(locality);
 		loca.click().perform();
 		Thread.sleep(6000);
 		
-				
-      // String parent = driver.getWindowHandle();
-		
-		//Set<String> pops=driver.getWindowHandles();
-		//{
-			//Iterator<String> it =pops.iterator();
-			//while (it.hasNext()) {
-				//String popupHandle=it.next().toString();
-				//if(!popupHandle.contains(parent))
-				//{
-				//	driver.switchTo().window(popupHandle);
-				//	System.out.println("Pop up Title: "+ driver.switchTo().window(popupHandle).getTitle());
-				//	driver.close();
-				//}
-			//}
-		//}
-		//driver.switchTo().alert().sendKeys("no");
-		
 		 WebElement survey=driver.findElement(By.xpath("//*[@id=\"survey-close-div\"]"));
-	     survey.click();
-	    // driver.findElement(By.xpath("//*[@id=\"surveyResponseFormId\"]/fieldset/div[2]/button")).click();
-		 Thread.sleep(3000);
-		// driver.findElement(By.xpath("//*[@id=\"survey-close-div\"]/i")).click();
-		// Thread.sleep(3000);
-		
+	         survey.click();
+	        Thread.sleep(3000);
+				
 		Actions star = new Actions(driver);
 		WebElement stars=driver.findElement(By.xpath("//*[@id=\"hlistpg_fr_star_category\"]/ul/li[3]/span[1]/label"));
 		star.moveToElement(stars);
@@ -174,7 +142,7 @@ public class Onlinebooking {
 		
 		driver.findElement(By.id("CC_tab")).click();
 		Thread.sleep(3000);
-		
+		//Payment Details
 		WebElement cardno=driver.findElement(By.id("PAYMENT_cardNumber"));
 		cardno.sendKeys("574896123451567");
 		WebElement cardname=driver.findElement(By.id("PAYMENT_nameOnCard"));
